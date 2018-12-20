1c01325e956aa2ad41ce5b8632712f928f55234c
Tecnico - TI <tecnico@epfserver.com.br>
2018-12-20 18:54:28 -0300

    Segundo commit

---
+++ src/main/java/br/com/loja/lojamusica/domain/Funcionarios.java
@@ -0,0 +1,12 @@
+package br.com.loja.lojamusica.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+
+
+@Entity
+@Table(name="funcionarios")
+public class Funcionarios {
+
+}
