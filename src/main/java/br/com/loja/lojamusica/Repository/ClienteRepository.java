ce9cc1b5a7c7cc71bf675de266f03259fd181f80
Tecnico - TI <tecnico@epfserver.com.br>
2019-01-09 19:50:47 -0300

    ultimo commit

--- src/main/java/br/com/loja/lojamusica/Repository/ClienteRepository.java
+++ src/main/java/br/com/loja/lojamusica/Repository/ClienteRepository.java
@@ -1,9 +1,16 @@
 package br.com.loja.lojamusica.Repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import br.com.loja.lojamusica.domain.Cliente;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
 
+	//HQL
+	@Query("SELECT c FROM Cliente c WHERE c.cpf = :cpf")
+	Optional<Cliente> findByCpf(@Param("cpf") String cpf);
 }
